94d5a7f0b27e0c84af58bba81f8923c475c7ac09
Ivan Zhang <ivanzhangofficial@gmail.com>
2024-11-25 18:39:13 -0800

    add exa

--- stalker/flow.py
+++ stalker/flow.py
@@ -6,7 +6,7 @@ import messages
 import database
 from dotenv import load_dotenv
 import os
-from stalk import stalk
+from stalk import stalk, stalk_category
 
 load_dotenv()
 
@@ -38,12 +38,48 @@ async def receive_msg(request: Request):
     message_history = database.get(fromNumber)
     if isinstance(inbound_msg, InboundTextMessage):
         if message_history and len(message_history) > 0:
-            pinn.send.rcs(from_=AGENT_ID, to=fromNumber, **stalk(inbound_msg.text))
-            pinn.send.rcs(from_=AGENT_ID, to=fromNumber, **messages.outro_msg)
-            print(f"Stalk message sent to {fromNumber}")
+            pinn.send.rcs(
+                from_=AGENT_ID,
+                to=fromNumber,
+                text=f"What would you like to know about '{inbound_msg.text}'?",
+                quick_replies=[
+                    Action(
+                        title="General Information", type="trigger", payload="general"
+                    ),
+                    Action(
+                        title="Personal Website",
+                        type="trigger",
+                        payload="personal site",
+                    ),
+                    Action(
+                        title="LinkedIn Profile",
+                        type="trigger",
+                        payload="linkedin profile",
+                    ),
+                    Action(title="Tweets", type="trigger", payload="tweet"),
+                    Action(title="News", type="trigger", payload="news"),
+                ],
+            )
         else:
             pinn.send.rcs(from_=AGENT_ID, to=fromNumber, **messages.intro_msg)
             print(f"Intro message sent to {fromNumber}")
+    elif isinstance(inbound_msg, InboundActionMessage):
+        category = inbound_msg.payload
+        prev_msg = message_history[-1] if message_history else None
+        if category and isinstance(prev_msg, InboundTextMessage):
+            if category == "general":
+                pinn.send.rcs(from_=AGENT_ID, to=fromNumber, **stalk(prev_msg.text))
+            else:
+                pinn.send.rcs(
+                    from_=AGENT_ID,
+                    to=fromNumber,
+                    **stalk_category(prev_msg.text, category=category),
+                )
+            pinn.send.rcs(from_=AGENT_ID, to=fromNumber, **messages.outro_msg)
+            print(f"Stalk message sent to {fromNumber}")
+        else:
+            pinn.send.rcs(from_=AGENT_ID, to=fromNumber, **messages.rcs_error_msg)
+
     else:
         pinn.send.rcs(from_=AGENT_ID, to=fromNumber, **messages.rcs_error_msg)
 
--- stalker/stalk.py
+++ stalker/stalk.py
@@ -1,7 +1,22 @@
-from rcs_types import RCSMessage, Action
+from rcs_types import RCSMessage, Action, Card
 from typing import List
 from openai import OpenAI
+from exa_py import Exa
 import os
+import requests
+
+
+def valdiate_citations(citations: List[str]) -> List[str]:
+    valid_citations = []
+    for citation in citations:
+        try:
+            response = requests.head(citation, allow_redirects=True)
+            if response.status_code == 200:
+                valid_citations.append(citation)
+        except requests.RequestException:
+            continue
+
+    return valid_citations
 
 
 def stalk(query: str) -> RCSMessage:
@@ -10,7 +25,7 @@ def stalk(query: str) -> RCSMessage:
     )
 
     # chat completion without streaming
-    response = client.chat.completions.create(
+    pplx_response = client.chat.completions.create(
         model="llama-3.1-sonar-large-128k-online",
         messages=[
             {
@@ -27,12 +42,48 @@ def stalk(query: str) -> RCSMessage:
         ],
     )
 
-    citations: List[str] = response.citations  # type: ignore
+    citations: List[str] = pplx_response.citations  # type: ignore
+
+    citations = valdiate_citations(citations)
 
     return {
-        "text": response.choices[0].message.content,
+        "text": pplx_response.choices[0].message.content,
         "quick_replies": [
             Action(title=f"Source {index + 1}", type="openUrl", payload=sourceUrl)
             for index, sourceUrl in enumerate(citations)
         ],
     }
+
+
+def stalk_category(query: str, category: str) -> RCSMessage:
+    exa = Exa(os.environ["EXA_API_KEY"])
+
+    # chat completion without streaming
+    response = exa.search_and_contents(
+        query=query,
+        use_autoprompt=True,
+        type="auto",
+        category=category,
+        num_results=5,
+        text={"include_html_tags": False, "max_characters": 100},
+    )
+
+    results = response.results
+
+    if len(results) == 0:
+        return {
+            "text": "Sorry, I didn't find anything. Is there anyone else I can find for you?",
+        }
+    else:
+        return {
+            "cards": [
+                Card(
+                    title=result.title or "",
+                    subtitle=result.text or "",
+                    buttons=[
+                        Action(title="Read More", type="openUrl", payload=result.url)
+                    ],
+                )
+                for result in results
+            ],
+        }
